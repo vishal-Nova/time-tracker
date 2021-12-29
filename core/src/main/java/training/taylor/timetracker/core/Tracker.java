1d5dd368a148309585e16dcf1d62a9d71a8f12d9
vishal-Nova <75784679+vishal-Nova@users.noreply.github.com>
2021-12-29 23:35:06 +0530

    Update Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -7,7 +7,7 @@ import training.taylor.timetracker.core.dao.TimeEntry;
 import java.util.List;
 
 @Component
-public cass Tracker {
+public class Tracker {
     @Autowired
     private List<TimeEntry> entries;
 
